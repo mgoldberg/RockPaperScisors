03f0cd4258a5e4338814df95455d5f7910787ccd
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-01 11:50:17 -0400

    added header and stylesheet

--- index.php
+++ index.php
@@ -6,11 +6,20 @@
 <!DOCTYPE HTML>
 
 	<head>
-		<title>Submition Form</title>
+		<title>Rock, Paper, Scisors</title>
 	</head>
 
 	<body>
 
+		<header id = "main">
+			<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+			Rock Paper Scisors
+		</header>
+
+		<div>
+
+		</div>
+
 	</body>
-	
+
 </html>
