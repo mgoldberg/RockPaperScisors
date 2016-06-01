01effca832552c50a858e88bd5bdce5a1926114d
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-01 13:51:27 -0400

    recod total thows

--- index.php
+++ index.php
@@ -1,37 +1,45 @@
 <?php
 
+
+
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$numberOfThrows = $_POST['numberOfThrows'];
+		$numberOfThrows++;
    		if (isset($_POST['rock-submitted'])) {
         	echo "You played rock.";
         	$computer = rand(1, 3);
         	if($computer == 1){
-        		echo " Tie!";
+        		echo " Tie!<br>";
         	} else if($computer == 2){
-        		echo " You lose!";
+        		echo " You lose!<br>";
         	} else if($computer == 3){
-        		echo " You win!";
+        		echo " You win!<br>";
         	}
+        	$numberOfThrows++;
    		} else if (isset($_POST['paper-submitted'])) {
         	echo "You played paper.";
         	$computer = rand(1, 3);
         	if($computer == 1){
-        		echo " You win!";
+        		echo " You win!<br>";
         	} else if($computer == 2){
-        		echo " Tie!";
+        		echo " Tie!<br>";
         	} else if($computer == 3){
-        		echo " You lose!";
+        		echo " You lose!<br>";
         	}
+        	$numberOfThrows++;
    		} else {
         	echo "You played scisors.";
         	$computer = rand(1, 3);
         	if($computer == 1){
-        		echo " You lose!";
+        		echo " You lose!<br>";
         	} else if($computer == 2){
-        		echo " You win!";
+        		echo " You win!<br>";
         	} else if($computer == 3){
-        		echo " Tie!";
+        		echo " Tie!<br>";
         	}
+        	$numberOfThrows++;
    		}
+   		echo "Number of total throws: ".$numberOfThrows;
 	}
 
 
@@ -58,15 +66,15 @@
 		</div>
 
 		<div id="rock">
-			<!--<input id="rock-submitted" type="hidden" name="rockSubmitted" value="true" />-->
+			<input type="hidden" name="numberOfThrows" value= "<?php echo $numberOfThrows ?>"/>
 			<input type="submit" name="rock-submitted" value="Rock">
 		</div>	
 		<div id="paper">
-			<!--<input id="paper-submitted" type="hidden" name="paperSubmitted" value="true" />-->
+			<input type="hidden" name="numberOfThrows" value= "<?php echo $numberOfThrows ?>"/>
 			<input type="submit" name="paper-submitted" value="Paper">
 		</div>
 		<div id="scisors">
-			<!--<input id="scisors-submitted" type="hidden" name="scisorsSubmitted" value="true" />-->
+			<input type="hidden" name="numberOfThrows" value= "<?php echo $numberOfThrows ?>"/>
 			<input type="submit" neam="scisors-submitted" value="Scisors">
 		</div>
 
